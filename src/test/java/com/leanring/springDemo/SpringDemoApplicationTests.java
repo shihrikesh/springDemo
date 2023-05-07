1a159ffa9b7badbb1028841fdb5a84625542b445
Hrishikesh_Tiwari <hrishi.jiet@gmail.com>
2023-05-07 18:07:33 +0530

    removing error code

--- src/test/java/com/leanring/springDemo/SpringDemoApplicationTests.java
+++ src/test/java/com/leanring/springDemo/SpringDemoApplicationTests.java
@@ -1,12 +1,7 @@
 package com.leanring.springDemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SpringDemoApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
